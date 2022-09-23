36ab9dbcb3e0b45650babd7b920318d46452a28d
phokok <igorehoho@gmail.com>
2022-09-22 18:21:34 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_square - prints a square
+* @size: is the size of the square
+*/
+
+void print_square(int size)
+
+{
+
+int row, column;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (ro = 0; row < size; row++)
+										{
+										for (column = 0; column < size;column++)
+																				{
+																				_putchar('#');
+																				}
+										_putcha('\n');																				}
+}
+}
